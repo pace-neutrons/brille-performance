0540fc3b9ea2bd7d2e892f4ed1e55698a70dce08
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2022-11-03 10:08:19 +0000

    Update powder map script and results

--- profile_scripts/powder_map_profile.py
+++ profile_scripts/powder_map_profile.py
@@ -1,28 +1,43 @@
-import os
 import time
 
+from pathlib import Path
+
 from euphonic.cli.powder_map import main as powder_map
+from euphonic import __version__ as eu_version
+from brille import version as br_version
 
 def main():
-    out_dir = os.path.join('..', 'profile_results', 'python', 'idaaas')
-    fc_dir = os.path.join('..', 'force_constants')
+
+    platform_name = 'windows_i711800H_32GB'
+    version_dirname = f'euphonic_{eu_version}_brille_{br_version}'.replace(
+        '+', '_')
+    out_dir = Path(
+        f'../profile_results/{version_dirname}/python/{platform_name}')
+    out_dir.mkdir(parents=True, exist_ok=True)
+    fc_dir = Path('../force_constants')
     n_repeats = 3
     fcs = ['quartz.castep_bin',
-           'Nb-181818-s0.5-NCP19-vib-disp.castep_bin']
-    materials = ['quartz', 'Nb']
+           'Nb-181818-s0.5-NCP19-vib-disp.castep_bin',
+           'La2Zr2O7.castep_bin',
+           'AmSulf_298K.castep_bin']
+    #fcs = ['AmSulf_298K.castep_bin']
+    materials = ['quartz', 'Nb', 'LZO', 'AmSulf']
+    #materials = ['AmSulf']
 
-    args = ['--n-threads', '30', '-s', 'tmp.png', '--npts-density', '10000']
+    args = ['--n-threads', '16', '-s', 'tmp.png', '--npts-density', '10000']
     sf_args = ['-w', 'coherent',
                '--grid', '6', '6', '6',
                '--temperature', '5']
-    brille_npts = ['10000', '20000']
-    dipole_params = ['0.75', '1.0']
+    brille_npts = ['10000', '20000', '7500', '500']
+    #brille_npts = ['500']
+    dipole_params = ['0.75', '1.0', '1.0', '0.4']
+    #dipole_params = ['0.4']
 
-    fobj = open(os.path.join(out_dir, f'powder_map_{int(time.time())}.txt'), 'w')
+    fobj = open(Path(f'{out_dir}/powder_map_{int(time.time())}.txt'), 'w')
     fobj.write(f'{sf_args} {args} --brille-npts={brille_npts} --dipole-parameter={dipole_params}')
 
     for i, fc in enumerate(fcs):
-        fci = os.path.join(fc_dir, fc)
+        fci = str(Path(f'{fc_dir}/{fc}'))
         fobj.write(f'\n\nMaterial: {materials[i]:10}\n')
         # Euphonic only - DOS weighted
         eu_args = [fci, *args, '--dipole-parameter', dipole_params[i]]
